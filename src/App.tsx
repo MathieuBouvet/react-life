87974ba5289bac4733b3afac597478b3100472c2
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-18 21:36:27 +0200

    Create the app theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
-const App = () => <div className="app"></div>;
+const theme = {
+  headerHeight: "75px",
+} as const;
+
+type Theme = typeof theme;
+
+declare module "styled-components" {
+  export interface DefaultTheme extends Theme {}
+}
+
+const App = () => (
+  <div className="app">
+    <ThemeProvider theme={theme}></ThemeProvider>
+  </div>
+);
 
 export default App;
