6fc11def2146da0122ce5b6039fb7a2d470453ae
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-20 23:21:18 +0200

    Add box-sizing to global style

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ declare module "styled-components" {
 }
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
