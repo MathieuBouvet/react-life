b82825d6e1cea56006c58ace5531474f860988cc
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-23 00:08:50 +0200

    Create the Cell component

---
+++ src/components/Life/Cell.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import { LifeAction } from "./lifeState";
+
+const StyledCell = styled.div<CellProps>`
+  background-color: ${({ theme }) => theme.colors.primaryLight};
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.highlight};
+  }
+`;
+
+type CellProps = {
+  alive: boolean;
+  position: [number, number];
+  dispatch: React.Dispatch<LifeAction>;
+};
+
+const Cell = (props: CellProps) => <StyledCell {...props} />;
+
+export default Cell;
