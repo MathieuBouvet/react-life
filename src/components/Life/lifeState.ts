e9a1c7a8bf164eb8570a6b5b08a77f4f244f7105
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-23 00:48:42 +0200

    Implement cell click action

--- src/components/Life/lifeState.ts
+++ src/components/Life/lifeState.ts
@@ -15,7 +15,12 @@ interface Start {
   type: "START";
 }
 
-export type LifeAction = Start | Iterate;
+interface CellClick {
+  type: "CELL_CLICK";
+  payload: { position: Cell };
+}
+
+export type LifeAction = Start | Iterate | CellClick;
 
 type LifeReducer = (prevState: LifeState, action: LifeAction) => LifeState;
 
@@ -26,6 +31,20 @@ const initialLife: LifeState = {
   liveCells: new Map([]),
 };
 
-const lifeReducer: LifeReducer = (prevState, action) => prevState;
+const lifeReducer: LifeReducer = (prevState, action) => {
+  switch (action.type) {
+    case "CELL_CLICK":
+      const cellsCopy = new Map(prevState.liveCells);
+      const position = action.payload.position;
+      if (cellsCopy.has(position)) {
+        cellsCopy.delete(position);
+      } else {
+        cellsCopy.set(position, true);
+      }
+      return { ...prevState, liveCells: cellsCopy };
+    default:
+      return prevState;
+  }
+};
 
 export { initialLife, lifeReducer };
