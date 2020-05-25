8a694882eea1e4edaabdfa0550f484b107722a1d
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-25 22:55:23 +0200

    Test next iteration util

---
+++ src/utils/gridManagement.test.ts
@@ -0,0 +1,97 @@
+import { nextIteration } from "./gridManagement";
+
+test.each<[string, [string, true][], [string, true][]]>([
+  [
+    "block",
+    [
+      ["1;1", true],
+      ["1;2", true],
+      ["2;1", true],
+      ["2;2", true],
+    ],
+    [
+      ["1;1", true],
+      ["1;2", true],
+      ["2;1", true],
+      ["2;2", true],
+    ],
+  ],
+  [
+    "bee-hive",
+    [
+      ["2;1", true],
+      ["1;2", true],
+      ["3;2", true],
+      ["1;3", true],
+      ["3;3", true],
+      ["2;4", true],
+    ],
+    [
+      ["2;1", true],
+      ["1;2", true],
+      ["3;2", true],
+      ["1;3", true],
+      ["3;3", true],
+      ["2;4", true],
+    ],
+  ],
+  [
+    "blinker",
+    [
+      ["2;1", true],
+      ["2;2", true],
+      ["2;3", true],
+    ],
+    [
+      ["1;2", true],
+      ["2;2", true],
+      ["3;2", true],
+    ],
+  ],
+  [
+    "toad",
+    [
+      ["2;2", true],
+      ["2;3", true],
+      ["2;4", true],
+      ["3;1", true],
+      ["3;2", true],
+      ["3;3", true],
+    ],
+    [
+      ["2;1", true],
+      ["3;1", true],
+      ["4;2", true],
+      ["1;3", true],
+      ["2;4", true],
+      ["3;4", true],
+    ],
+  ],
+  [
+    "beacon",
+    [
+      ["1;1", true],
+      ["2;1", true],
+      ["1;2", true],
+      ["4;3", true],
+      ["3;4", true],
+      ["4;4", true],
+    ],
+    [
+      ["1;1", true],
+      ["2;1", true],
+      ["1;2", true],
+      ["2;2", true],
+      ["3;3", true],
+      ["4;3", true],
+      ["3;4", true],
+      ["4;4", true],
+    ],
+  ],
+  ["nothing", [], []],
+])("two iterations: %s", (_, previousData, nextData) => {
+  const previous = new Map(previousData);
+  const next = new Map(nextData);
+  expect(nextIteration(previous, 6, 6)).toEqual(next);
+  expect(nextIteration(next, 6, 6)).toEqual(previous);
+});
