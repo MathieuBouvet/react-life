fe561797c3d803752201271d3040941350baaa96
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-26 16:59:41 +0200

    Move theme and global style to dedicated file

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,7 @@
 import React from "react";
-import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import Life from "./Life";
-
-const theme = {
-  headerHeight: "75px",
-  toolBarWidth: "240px",
-  colors: {
-    primary: "#017927",
-    primaryLight: "#96e072",
-    primaryDark: "#134611",
-    light: "#fff",
-    dark: "#2A2B2A",
-    highlight: "#eec643",
-  },
-} as const;
-
-type Theme = typeof theme;
-
-declare module "styled-components" {
-  export interface DefaultTheme extends Theme {}
-}
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    margin: 0;
-    color: ${props => props.theme.colors.dark};
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-`;
+import { theme, GlobalStyle } from "./theme";
 
 const StyledApp = styled.section`
   display: grid;
---
+++ src/theme.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+const theme = {
+  headerHeight: "75px",
+  toolBarWidth: "240px",
+  colors: {
+    primary: "#017927",
+    primaryLight: "#96e072",
+    primaryDark: "#134611",
+    light: "#fff",
+    dark: "#2A2B2A",
+    highlight: "#eec643",
+  },
+} as const;
+
+type Theme = typeof theme;
+
+declare module "styled-components" {
+  export interface DefaultTheme extends Theme {}
+}
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    color: ${props => props.theme.colors.dark};
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+export { theme, GlobalStyle };
