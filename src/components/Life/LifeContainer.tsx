87278ecef7955538e5bafa80ad254de5fbe2fe0f
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-26 12:51:10 +0200

    Schedule next iteration

--- src/components/Life/LifeContainer.tsx
+++ src/components/Life/LifeContainer.tsx
@@ -1,9 +1,19 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { lifeReducer, initialLife } from "./lifeState";
 import LifeDisplay from "./LifeDisplay";
 
 const LifeContainer = () => {
   const [lifeState, dispatchLife] = useReducer(lifeReducer, initialLife);
+  useEffect(() => {
+    const nextIterationTimer = setTimeout(() => {
+      if (lifeState.started) {
+        dispatchLife({ type: "ITERATE" });
+      }
+    }, 50);
+    return () => {
+      clearTimeout(nextIterationTimer);
+    };
+  });
   return <LifeDisplay {...{ ...lifeState, dispatch: dispatchLife }} />;
 };
 
