157784271ddd6474ce7a1f307a53244ffe906b42
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-29 18:34:18 +0200

    Style button

--- src/Buttons.tsx
+++ src/Buttons.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { LifeAction } from "./lifeState";
 import { FiPlayCircle } from "react-icons/fi";
 
@@ -11,13 +12,39 @@ export type SpecializedButtonProps = {
   dispatch: React.Dispatch<LifeAction>;
 };
 
+const StyledButton = styled.button`
+  display: block;
+  border: none;
+  background-color: inherit;
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+  font-family: inherit;
+  font-size: 1rem;
+  cursor: pointer;
+  text-align: center;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+
+  &:hover > :first-child {
+    transform: scale(1.075);
+  }
+
+  & * {
+    display: block;
+  }
+`;
+
 const Button = ({ action, dispatch, children }: ButtonProps) => (
-  <button onClick={() => dispatch(action)}>{children}</button>
+  <StyledButton onClick={() => dispatch(action)}>{children}</StyledButton>
 );
 
 const StartButton = ({ dispatch }: SpecializedButtonProps) => (
   <Button dispatch={dispatch} action={{ type: "START" }}>
-    <FiPlayCircle />
+    <>
+      <FiPlayCircle size="4.5rem" />
+      start
+    </>
   </Button>
 );
 
