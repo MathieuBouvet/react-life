e1e9cb238243fa07d3d0346ed56ff0937840cfc3
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-06-03 11:25:53 +0200

    Test optimized generation algorithm

--- src/utils/gridManagement.test.ts
+++ src/utils/gridManagement.test.ts
@@ -1,4 +1,4 @@
-import { nextIteration } from "./gridManagement";
+import { nextIteration, nextIterationOptimized } from "./gridManagement";
 
 test.each<[string, [string, true][], [string, true][]]>([
   [
@@ -94,4 +94,100 @@ test.each<[string, [string, true][], [string, true][]]>([
   const next = new Map(nextData);
   expect(nextIteration(previous, 6, 6)).toEqual(next);
   expect(nextIteration(next, 6, 6)).toEqual(previous);
+});
+
+test.each<[string, [string, true][], [string, true][]]>([
+  [
+    "optimized : block",
+    [
+      ["1;1", true],
+      ["1;2", true],
+      ["2;1", true],
+      ["2;2", true],
+    ],
+    [
+      ["1;1", true],
+      ["1;2", true],
+      ["2;1", true],
+      ["2;2", true],
+    ],
+  ],
+  [
+    "optimized : bee-hive",
+    [
+      ["2;1", true],
+      ["1;2", true],
+      ["3;2", true],
+      ["1;3", true],
+      ["3;3", true],
+      ["2;4", true],
+    ],
+    [
+      ["2;1", true],
+      ["1;2", true],
+      ["3;2", true],
+      ["1;3", true],
+      ["3;3", true],
+      ["2;4", true],
+    ],
+  ],
+  [
+    "optimized : blinker",
+    [
+      ["2;1", true],
+      ["2;2", true],
+      ["2;3", true],
+    ],
+    [
+      ["1;2", true],
+      ["2;2", true],
+      ["3;2", true],
+    ],
+  ],
+  [
+    "optimized : toad",
+    [
+      ["2;2", true],
+      ["2;3", true],
+      ["2;4", true],
+      ["3;1", true],
+      ["3;2", true],
+      ["3;3", true],
+    ],
+    [
+      ["2;1", true],
+      ["3;1", true],
+      ["4;2", true],
+      ["1;3", true],
+      ["2;4", true],
+      ["3;4", true],
+    ],
+  ],
+  [
+    "optimized : beacon",
+    [
+      ["1;1", true],
+      ["2;1", true],
+      ["1;2", true],
+      ["4;3", true],
+      ["3;4", true],
+      ["4;4", true],
+    ],
+    [
+      ["1;1", true],
+      ["2;1", true],
+      ["1;2", true],
+      ["2;2", true],
+      ["3;3", true],
+      ["4;3", true],
+      ["3;4", true],
+      ["4;4", true],
+    ],
+  ],
+  ["optimized : nothing", [], []],
+])("two iterations: %s", (_, previousData, nextData) => {
+  const previous = new Map(previousData);
+  const next = new Map(nextData);
+  expect(nextIterationOptimized(previous)).toEqual(next);
+  expect(nextIterationOptimized(next)).toEqual(previous);
 });
