90f0a87b7bb6496dd20cb89b35158fe582d1ac83
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-06-06 23:39:37 +0200

    Fix cell size to constant

--- src/Life/Cell.tsx
+++ src/Life/Cell.tsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { LifeAction, CellPosition } from "../lifeState";
+import { BASE_CELL_SIZE, LifeAction, CellPosition } from "../lifeState";
 import { Rect } from "react-konva/lib/ReactKonvaCore";
 import "konva/lib/shapes/Rect";
 import { theme } from "../theme";
 
 type CellProps = {
   alive: boolean;
-  size: number;
   position: CellPosition;
   dispatch: React.Dispatch<LifeAction>;
 };
 
-const Cell = ({ alive, position, dispatch, size }: CellProps) => {
+const Cell = ({ alive, position, dispatch }: CellProps) => {
   return (
     <Rect
-      x={position[1] * (size + 1)}
-      y={position[0] * (size + 1)}
-      width={size}
-      height={size}
+      x={position[1] * (BASE_CELL_SIZE + 1)}
+      y={position[0] * (BASE_CELL_SIZE + 1)}
+      width={BASE_CELL_SIZE}
+      height={BASE_CELL_SIZE}
       fill={alive ? theme.colors.primary : theme.colors.primaryLight}
       onMouseDown={() =>
         dispatch({ type: "TOGGLE_CELL", payload: { position } })
@@ -35,8 +34,7 @@ const CellMemo = React.memo(Cell, (prev, next) => {
   return (
     prev.position[0] === next.position[0] &&
     prev.position[1] === next.position[1] &&
-    prev.alive === next.alive &&
-    prev.size === next.size
+    prev.alive === next.alive
   );
 });
 
