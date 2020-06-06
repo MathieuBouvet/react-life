447e3587a1b44da7b0d17cc0fddda78f4c1a2bb6
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-06-06 17:26:26 +0200

    Clean unused styling

--- src/Life/Cell.tsx
+++ src/Life/Cell.tsx
@@ -1,26 +1,8 @@
 import React from "react";
-import styled from "styled-components";
 import { LifeAction, CellPosition } from "../lifeState";
 import { Rect } from "react-konva/lib/ReactKonvaCore";
 import "konva/lib/shapes/Rect";
 import { theme } from "../theme";
-
-type StyledProps = Pick<CellProps, "alive" | "size" | "position">;
-
-const StyledCell = styled.rect.attrs<StyledProps>(props => ({
-  width: props.size,
-  height: props.size,
-  y: props.position[0] * (props.size + 1),
-  x: props.position[1] * (props.size + 1),
-}))<StyledProps>`
-  fill: ${({ alive, theme }) =>
-    alive ? theme.colors.primary : theme.colors.primaryLight};
-  cursor: pointer;
-  &:hover {
-    fill: ${({ alive, theme }) =>
-      alive ? theme.colors.primary : theme.colors.highlight};
-  }
-`;
 
 type CellProps = {
   alive: boolean;
