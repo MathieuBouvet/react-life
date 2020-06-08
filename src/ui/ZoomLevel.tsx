d6f43fafe970c46b7c824e3a140ec77cd1a15dcb
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-06-08 17:47:23 +0200

    Add icon to zoom input

--- src/ui/ZoomLevel.tsx
+++ src/ui/ZoomLevel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { LifeAction } from "../lifeState";
+import { TiZoomOutline } from "react-icons/ti";
 
 type ZoomLevelProps = {
   value: number;
@@ -16,9 +17,38 @@ const ZoomLevelInput = styled.input.attrs<ZoomLevelProps>(props => ({
       payload: { zoomLevel: parseInt(e.target.value, 10) },
     }),
 }))<ZoomLevelProps>`
+  border: none;
+  border-radius: 0;
+  text-align: center;
+  height: 100%;
+  width: 60px;
+
   &:focus {
     outline: none;
   }
 `;
 
-export default ZoomLevelInput;
+const StyledZoomLevel = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  border: 1px solid gray;
+  border-radius: 2px;
+  height: 30px;
+`;
+
+const ZoomIcon = styled(TiZoomOutline).attrs(props => ({
+  size: "1.5em",
+}))`
+  background-color: white;
+  color: ${props => props.theme.colors.dark};
+  height: 100%;
+`;
+
+const ZoomLevel = (props: ZoomLevelProps) => (
+  <StyledZoomLevel>
+    <ZoomIcon />
+    <ZoomLevelInput {...props} />
+  </StyledZoomLevel>
+);
+
+export default ZoomLevel;
