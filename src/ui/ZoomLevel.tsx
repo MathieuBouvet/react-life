1a6eede8f4ec993de766a3ab9bd2fdb9c9a58277
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-06-08 17:01:01 +0200

    Avoid displaying js rounding errors

--- src/ui/ZoomLevel.tsx
+++ src/ui/ZoomLevel.tsx
@@ -9,12 +9,16 @@ type ZoomLevelProps = {
 const ZoomLevelInput = styled.input.attrs<ZoomLevelProps>(props => ({
   type: "number",
   step: 5,
-  value: props.value * 100,
+  value: Math.round(props.value * 100),
   onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
     props.dispatch({
       type: "SET_ZOOM_LEVEL",
       payload: { zoomLevel: parseInt(e.target.value, 10) },
     }),
-}))<ZoomLevelProps>``;
+}))<ZoomLevelProps>`
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default ZoomLevelInput;
