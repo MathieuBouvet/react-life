7d2f1e85e6068808e1c3e2aa6a01160cdc455588
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-06-12 01:05:25 +0200

    Simplify Cell component
    A rendered cell can only be alive now

--- src/Life/Cell.tsx
+++ src/Life/Cell.tsx
@@ -1,31 +1,21 @@
 import React from "react";
-import { BASE_CELL_SIZE, LifeAction, CellPosition } from "../lifeState";
+import { BASE_CELL_SIZE, CellPosition } from "../lifeState";
 import { Rect } from "react-konva/lib/ReactKonvaCore";
 import "konva/lib/shapes/Rect";
 import { theme } from "../theme";
 
 type CellProps = {
-  alive: boolean;
   position: CellPosition;
-  dispatch: React.Dispatch<LifeAction>;
 };
 
-const Cell = ({ alive, position, dispatch }: CellProps) => {
+const Cell = ({ position }: CellProps) => {
   return (
     <Rect
       x={position[1] * (BASE_CELL_SIZE + 1)}
       y={position[0] * (BASE_CELL_SIZE + 1)}
       width={BASE_CELL_SIZE}
       height={BASE_CELL_SIZE}
-      fill={alive ? theme.colors.primary : theme.colors.primaryLight}
-      onMouseDown={() =>
-        dispatch({ type: "TOGGLE_CELL", payload: { position } })
-      }
-      onMouseEnter={e => {
-        if (e.evt.buttons === 1) {
-          dispatch({ type: "SET_CELL_ALIVE", payload: { position } });
-        }
-      }}
+      fill={theme.colors.primary}
     />
   );
 };
@@ -33,8 +23,7 @@ const Cell = ({ alive, position, dispatch }: CellProps) => {
 const CellMemo = React.memo(Cell, (prev, next) => {
   return (
     prev.position[0] === next.position[0] &&
-    prev.position[1] === next.position[1] &&
-    prev.alive === next.alive
+    prev.position[1] === next.position[1]
   );
 });
 
