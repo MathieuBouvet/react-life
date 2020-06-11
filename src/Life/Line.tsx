fc4bc0deb1d6186bd57f96b87da422441e0f2503
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-06-11 23:28:24 +0200

    Create Lines component

---
+++ src/Life/Line.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Rect } from "react-konva/lib/ReactKonvaCore";
+import "konva/lib/shapes/Rect";
+import { GRID_SIZE, BASE_CELL_SIZE } from "../lifeState";
+import { theme } from "../theme";
+
+type LineProps = {
+  offset: number;
+};
+
+const VerticalLine = ({ offset }: LineProps) => (
+  <Rect
+    x={offset}
+    y={0}
+    width={1}
+    height={GRID_SIZE * (BASE_CELL_SIZE + 1)}
+    fill={theme.colors.primaryLight}
+  />
+);
+
+const HorizontalLine = ({ offset }: LineProps) => (
+  <Rect
+    x={0}
+    y={offset}
+    width={GRID_SIZE * (BASE_CELL_SIZE + 1)}
+    height={1}
+    fill={theme.colors.primaryLight}
+  />
+);
+
+export { VerticalLine, HorizontalLine };
