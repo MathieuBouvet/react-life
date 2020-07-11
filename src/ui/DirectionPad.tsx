6cfed6054ecf1b9d80126c388096a45625ec8a1f
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-07-11 14:38:33 +0200

    Create Direction Pad Coponent

---
+++ src/ui/DirectionPad.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import { LifeAction } from "../lifeState";
+import { ArrowButton } from "./Buttons";
+
+type DirectionPadProps = {
+  dispatch: React.Dispatch<LifeAction>;
+};
+
+const StyledDirectionPad = styled.div<DirectionPadProps>`
+  display: grid;
+  width: 75%;
+  margin-top: auto;
+  grid-template-areas:
+    "UP UP"
+    "LEFT RIGHT"
+    "DOWN DOWN";
+`;
+
+const Arrow = styled(ArrowButton)`
+  grid-area: ${props => props.direction};
+`;
+
+const DirectionPad = ({ dispatch }: DirectionPadProps) => (
+  <StyledDirectionPad dispatch={dispatch}>
+    <Arrow direction="UP" dispatch={dispatch} />
+    <Arrow direction="LEFT" dispatch={dispatch} />
+    <Arrow direction="RIGHT" dispatch={dispatch} />
+    <Arrow direction="DOWN" dispatch={dispatch} />
+  </StyledDirectionPad>
+);
+
+export default DirectionPad;
