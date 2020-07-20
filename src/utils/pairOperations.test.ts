66d5c156ff9958d0bf903c01c06f6d35c7ebb868
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-07-20 14:07:25 +0200

    Test divide pair

--- src/utils/pairOperations.test.ts
+++ src/utils/pairOperations.test.ts
@@ -1,4 +1,4 @@
-import { addPair, substractPair } from "./pairOperations";
+import { addPair, substractPair, dividePair } from "./pairOperations";
 
 describe("Pair addition", () => {
   test.each<[[number, number], [number, number] | number, [number, number]]>([
@@ -59,5 +59,31 @@ describe("Pair Substraction", () => {
     [[-5, -25], -30, [25, 5]],
   ])("%j - %j = %j", (p1, p2, result) => {
     expect(substractPair(p1, p2)).toStrictEqual(result);
+  });
+
+  describe("Pair division", () => {
+    test.each<[[number, number], [number, number] | number, [number, number]]>([
+      [
+        [5, 6],
+        [1, 1],
+        [5, 6],
+      ],
+      [
+        [0, 0],
+        [4, 5],
+        [0, 0],
+      ],
+      [
+        [51, 63],
+        [102, 3],
+        [0.5, 21],
+      ],
+      [[0, 0], 1, [0, 0]],
+      [[5, -5], 5, [1, -1]],
+      [[12, -24], 6, [2, -4]],
+      [[42, 42], 0, [Infinity, Infinity]],
+    ])("%j - %j = %j", (p1, p2, result) => {
+      expect(dividePair(p1, p2)).toStrictEqual(result);
+    });
   });
 });
