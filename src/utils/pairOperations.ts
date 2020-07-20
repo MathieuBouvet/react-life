ed39348151baf8aecc02184c996fdd3336e13807
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-07-20 13:56:50 +0200

    Improve pair operation flexibility
    Second parameter can be a single value, wich is converted to a pair of two
    values

--- src/utils/pairOperations.ts
+++ src/utils/pairOperations.ts
@@ -1,9 +1,19 @@
 export type Pair<T> = [T, T];
 
+function isPair<T>(p: any): p is Pair<T> {
+  return p.length === 2 && typeof p[0] === typeof p[1];
+}
+
 function pairOperation<T>(
   operation: (x: T, y: T) => T
-): (p1: Pair<T>, p2: Pair<T>) => Pair<T> {
-  return (p1, p2) => [operation(p1[0], p2[0]), operation(p1[1], p2[1])];
+): (p1: Pair<T>, p2: Pair<T> | T) => Pair<T> {
+  return (p1, p2) => {
+    const secondOperand = !isPair(p2) ? ([p2, p2] as Pair<T>) : p2;
+    return [
+      operation(p1[0], secondOperand[0]),
+      operation(p1[1], secondOperand[1]),
+    ];
+  };
 }
 
 const addPair = pairOperation<number>((x, y) => x + y);
