ae566ee88a5249f4e03a8cc37de02421144c8e5a
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-07-20 13:57:35 +0200

    Test pair operation second parameter flexibility

--- src/utils/pairOperations.test.ts
+++ src/utils/pairOperations.test.ts
@@ -1,7 +1,7 @@
 import { addPair, substractPair } from "./pairOperations";
 
 describe("Pair addition", () => {
-  test.each<[[number, number], [number, number], [number, number]]>([
+  test.each<[[number, number], [number, number] | number, [number, number]]>([
     [
       [0, 0],
       [0, 0],
@@ -22,13 +22,17 @@ describe("Pair addition", () => {
       [10, -30],
       [5, -55],
     ],
+    [[0, 0], 0, [0, 0]],
+    [[0, 0], 5, [5, 5]],
+    [[78, 42], 102, [180, 144]],
+    [[-5, -25], -20, [-25, -45]],
   ])("%j + %j = %j", (p1, p2, result) => {
     expect(addPair(p1, p2)).toStrictEqual(result);
   });
 });
 
 describe("Pair Substraction", () => {
-  test.each<[[number, number], [number, number], [number, number]]>([
+  test.each<[[number, number], [number, number] | number, [number, number]]>([
     [
       [0, 0],
       [0, 0],
@@ -49,6 +53,10 @@ describe("Pair Substraction", () => {
       [10, -30],
       [-15, 5],
     ],
+    [[0, 0], 0, [0, 0]],
+    [[0, 0], 5, [-5, -5]],
+    [[78, 42], 102, [-24, -60]],
+    [[-5, -25], -30, [25, 5]],
   ])("%j - %j = %j", (p1, p2, result) => {
     expect(substractPair(p1, p2)).toStrictEqual(result);
   });
