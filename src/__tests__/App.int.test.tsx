8f46733d4ad4ef11da990649e80a954db36b4ec9
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-07-20 13:49:41 +0200

    Remove unecessary file

--- src/__tests__/App.int.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "../App";
-
-test("App renders its title", () => {
-  const { queryByText } = render(<App />);
-  const appTitle = queryByText(/react life/i);
-  expect(appTitle).toBeInTheDocument();
-});
