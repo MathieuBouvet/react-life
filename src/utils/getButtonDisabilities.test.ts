36dc16301cdd898cd6198d286a93bb63db6c68fd
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-09-03 15:12:53 +0200

    Add more tests to button disabilities util

--- src/utils/getButtonDisabilities.test.ts
+++ src/utils/getButtonDisabilities.test.ts
@@ -16,6 +16,90 @@ test.each<[string, ButtonDisabilitiesParameters, ButtonDisabilities]>([
       undoDisabled: true,
     },
   ],
+  [
+    "simulation started and current edition is not empty",
+    [true, 0, 0, 42],
+    {
+      startDisabled: true,
+      pauseDisabled: false,
+      clearGridDisabled: false,
+      nextGenDisabled: true,
+      redoDisabled: true,
+      undoDisabled: true,
+    },
+  ],
+  [
+    "simulation started and we are at the end of edition stack",
+    [true, 21, 22, 42],
+    {
+      startDisabled: true,
+      pauseDisabled: false,
+      clearGridDisabled: false,
+      nextGenDisabled: true,
+      redoDisabled: true,
+      undoDisabled: true,
+    },
+  ],
+  [
+    "simulation started and we are not the end of edition stack",
+    [true, 5, 22, 42],
+    {
+      startDisabled: true,
+      pauseDisabled: false,
+      clearGridDisabled: false,
+      nextGenDisabled: true,
+      redoDisabled: true,
+      undoDisabled: true,
+    },
+  ],
+  [
+    "simulation not started",
+    [false, 0, 0, 0],
+    {
+      startDisabled: false,
+      pauseDisabled: true,
+      clearGridDisabled: true,
+      nextGenDisabled: false,
+      redoDisabled: true,
+      undoDisabled: true,
+    },
+  ],
+  [
+    "simulation not started and current edition is not empty",
+    [false, 0, 0, 42],
+    {
+      startDisabled: false,
+      pauseDisabled: true,
+      clearGridDisabled: false,
+      nextGenDisabled: false,
+      redoDisabled: true,
+      undoDisabled: true,
+    },
+  ],
+  [
+    "simulation not started and we are at the end of edition stack",
+    [false, 15, 16, 42],
+    {
+      startDisabled: false,
+      pauseDisabled: true,
+      clearGridDisabled: false,
+      nextGenDisabled: false,
+      redoDisabled: true,
+      undoDisabled: false,
+    },
+  ],
+  [
+    "simulation not started and we are not at the end of edition stack",
+    [false, 11, 16, 42],
+    {
+      startDisabled: false,
+      pauseDisabled: true,
+      clearGridDisabled: false,
+      nextGenDisabled: false,
+      redoDisabled: false,
+      undoDisabled: false,
+    },
+  ],
 ])("%s", (_, args, expected) => {
   expect(getButtonDisabilities(...args)).toEqual(expected);
 });
