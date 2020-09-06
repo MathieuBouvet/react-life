ae9e53f53e56bf05d5694d1642130299b93799b3
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-09-06 15:05:40 +0200

    Add base style and behavior

--- src/InfoPage.tsx
+++ src/InfoPage.tsx
@@ -3,8 +3,21 @@ import styled from "styled-components";
 
 type InfoPageProps = { visible: boolean };
 
-const StyledInfoPage = styled.div``;
+const StyledInfoPage = styled.div<InfoPageProps>`
+  position: absolute;
+  width: 100vw;
+  height: ${props => `calc(100vh - ${props.theme.headerHeight})`};
+  background-color: ${props => props.theme.colors.light};
+  top: ${props => props.theme.headerHeight};
+  left: 0;
+  transform: ${props => `translateX(${props.visible ? 0 : "100vw"})`};
+  transition: transform 500ms;
+`;
 
-const InfoPage = (props: InfoPageProps) => <StyledInfoPage></StyledInfoPage>;
+const InfoPage = (props: InfoPageProps) => (
+  <StyledInfoPage {...props}>
+    <h1>Info Page</h1>
+  </StyledInfoPage>
+);
 
 export default InfoPage;
