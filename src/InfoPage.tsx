561137a4c1cda822a2fc15716bab90ad7abf1f26
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-09-06 14:39:54 +0200

    Creatye base info page component

---
+++ src/InfoPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+type InfoPageProps = { visible: boolean };
+
+const StyledInfoPage = styled.div``;
+
+const InfoPage = (props: InfoPageProps) => <StyledInfoPage></StyledInfoPage>;
+
+export default InfoPage;
