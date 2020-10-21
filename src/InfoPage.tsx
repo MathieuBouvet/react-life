ab085498c80f1dd5579d5e807ae10f95fef789b4
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-10-21 10:56:51 +0200

    Add informations

--- src/InfoPage.tsx
+++ src/InfoPage.tsx
@@ -16,7 +16,33 @@ const StyledInfoPage = styled.div<InfoPageProps>`
 
 const InfoPage = (props: InfoPageProps) => (
   <StyledInfoPage {...props}>
-    <h1>Info Page</h1>
+    <h2>C'est quoi React Life ?</h2>
+    <p>React Life est une implementation du jeu de la vie.</p>
+    <p>
+      Le jeu de la vie est un automate cellulaire inventé par par John Horton
+      Conway en 1970.{" "}
+      <p>
+        Pour plus d'informations sur le jeu de la vie, voici la page wikipedia :{" "}
+        <a href="https://fr.wikipedia.org/wiki/Jeu_de_la_vie">
+          le jeu de la vie sur wikipedia
+        </a>{" "}
+        et une excellente vidéo youtube :{" "}
+        <a href="https://www.youtube.com/watch?v=S-W0NX97DB0">
+          le jeu de la vie expliqué par science etonnante
+        </a>
+      </p>
+    </p>
+    <h2>Comment utiliser React Life ?</h2>
+    <h3>Les contrôles :</h3>
+    <p>Cliquer pour activer ou désactiver une cellule</p>
+    <p>
+      Maintenir le clic et déplacer la souris pour activer des cellule
+      rapidement
+    </p>
+    <p>Utiliser la molette de la souris pour zoomer ou dézoomer</p>
+    <p>Utiliser les fèches directionelles du clavier pour déplacer la grille</p>
+    <h3>La barre d'outils :</h3>
+    <p></p>
   </StyledInfoPage>
 );
 
